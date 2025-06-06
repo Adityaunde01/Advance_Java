9b944dab638c52e71237ba402d2bd794ab8282a4
dac <dac@host.docker.internal>
2025-06-06 16:34:30 +0530

    formatted code

--- FirstAssignment/src/org/pcsix/db/Questoin1Util.java
+++ FirstAssignment/src/org/pcsix/db/Questoin1Util.java
@@ -9,7 +9,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public interface Questoin1Util {
-	
+
 	public static void printAllUsers(Connection dbConnection) throws SQLException{
 		Statement stSelect = dbConnection.createStatement();
 		ResultSet result  = stSelect.executeQuery("select * from users");
@@ -20,33 +20,33 @@ public interface Questoin1Util {
 			System.out.println();
 		}
 	}
-	
-	
+
+
 	public static void updatePass(Connection dbConnection ,Scanner sc) throws SQLException {
 		PreparedStatement psUpdate = dbConnection.prepareStatement("update users set password=? where username=?");
 		System.out.println("Enter UserName whose password to be updated: ");
 		String name = sc.nextLine();
 		System.out.println("Update the Password");
 		String password = sc.nextLine();
-		
-		
+
+
 		psUpdate.setString(1, password);
 		psUpdate.setString(2, name);
-		
+
 		psUpdate.executeUpdate();
 		System.out.println("Updated succesfully");
-		
+
 	}
-	
+
 	public static void printUserByCity(Connection dbConnection ,Scanner sc) throws SQLException {
 		PreparedStatement psSelect = dbConnection.prepareStatement("select * from users where city=?");
 		sc.nextLine();
 		System.out.println("enter City to see uses: ");
 		String city = sc.nextLine();
-		
-	
+
+
 		psSelect.setString(1, city);
-		
+
 		ResultSet result =  psSelect.executeQuery();
 		while(result.next()) {
 			for(int i =1; i<=6;i++) {
@@ -54,20 +54,20 @@ public interface Questoin1Util {
 			}
 			System.out.println();
 		}
-		
+
 		System.out.println("details shown succesfully");
-		
+
 	}
-	
+
 	public static void printUserDetails(Connection dbConnection ,Scanner sc) throws SQLException {
 		PreparedStatement psSelect = dbConnection.prepareStatement("select * from users where username=?");
 
 		System.out.println("enter username to see user details: ");
 		String username = sc.nextLine();
-		
-	
+
+
 		psSelect.setString(1, username);
-		
+
 		ResultSet result =  psSelect.executeQuery();
 		while(result.next()) {
 			for(int i =1; i<=6;i++) {
@@ -75,11 +75,11 @@ public interface Questoin1Util {
 			}
 			System.out.println();
 		}
-		
+
 		System.out.println("details shown succesfully");
-		
+
 	}
-	
+
 	public static void addUser(Connection dbConnection,Scanner sc) throws SQLException,InputMismatchException {
 		PreparedStatement psInsert = dbConnection.prepareStatement("insert into users values(?,?,?,?,?,?)");
 		System.out.println("Enter Id");
@@ -101,10 +101,10 @@ public interface Questoin1Util {
 		psInsert.setString(4, password);
 		psInsert.setString(5, city);
 		psInsert.setString(6, email);
-		
+
 		psInsert.executeUpdate();
 		System.out.println("added succesfully");
-		
+
 	}
 
 }
