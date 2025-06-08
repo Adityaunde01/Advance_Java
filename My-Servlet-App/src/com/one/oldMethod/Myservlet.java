69cea6aff2c5948f9bf6233834b670312f55dac9
GURUGASTGAR <gurugastgar@gmail.com>
2025-06-08 14:52:07 +0530

    implemented servlet with old method

--- My-Servlet-App/src/com/one/oldMethod/Myservlet.java
+++ My-Servlet-App/src/com/one/oldMethod/Myservlet.java
@@ -16,6 +16,7 @@ public class Myservlet extends HttpServlet {
         
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
+       
         
         out.println("<!DOCTYPE html>");
         out.println("<html>");
