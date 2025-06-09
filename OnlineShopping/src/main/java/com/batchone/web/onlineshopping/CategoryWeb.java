75e1a191e4c9891c5c58891ff6d5ce302e65769d
dac <dac@host.docker.internal>
2025-06-09 21:37:34 +0530

    edited catagory

--- OnlineShopping/src/main/java/com/batchone/web/onlineshopping/CategoryWeb.java
+++ OnlineShopping/src/main/java/com/batchone/web/onlineshopping/CategoryWeb.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Servlet implementation class CategoryWeb
@@ -27,7 +28,7 @@ public class CategoryWeb extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+		 PrintWriter out = response.getWriter();
 	}
 
 }
