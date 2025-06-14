762813cbe172d74f936a9fb787c70b0427d1b7b5
dac <dac@host.docker.internal>
2025-06-14 19:32:58 +0530

    added a functionality

--- HibernateMavenProject/src/main/java/com/example/User.java
+++ HibernateMavenProject/src/main/java/com/example/User.java
@@ -6,32 +6,32 @@ import javax.persistence.*;
 @Table(name = "users")
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 
-    private String name;
-    private String email;
+	private String name;
+	private String email;
 
-    public User() {}
+	public User() {}
 
-    public User(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
+	public User(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
 
-    // Getters and Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+	// Getters and Setters
+	public int getId() { return id; }
+	public void setId(int id) { this.id = id; }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+	public String getName() { return name; }
+	public void setName(String name) { this.name = name; }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+	public String getEmail() { return email; }
+	public void setEmail(String email) { this.email = email; }
 
-    @Override
-    public String toString() {
-        return "User [id=" + id + ", name=" + name + ", email=" + email + "]";
-    }
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + "]";
+	}
 }
